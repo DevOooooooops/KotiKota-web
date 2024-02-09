5d3d84da71a6ab1e84d205e7313d8a1d1727bb57
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-02-09 23:39:57 +0300

    docs: update readme

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,31 +1,25 @@
-import {
-  User,
-  UserCredential,
-  signInWithEmailAndPassword,
-} from "firebase/auth";
-import NextAuth from "next-auth/next";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { ICredential } from "./type";
-import { auth } from "@/app/provider";
+import { User, UserCredential, signInWithEmailAndPassword } from 'firebase/auth';
+import NextAuth from 'next-auth/next';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { ICredential } from './type';
+import { auth } from '@/app/provider';
 
 export const authOptions = {
   pages: {
-    signIn: "/signIn",
+    signIn: '/signIn',
   },
   providers: [
     CredentialsProvider({
-      name: "Credentials",
+      name: 'Credentials',
       credentials: {},
       async authorize(_credentials): Promise<any> {
         const credentials = _credentials as ICredential;
-        return await signInWithEmailAndPassword(
-          auth,
-          credentials.email,
-          credentials.password
-        ).catch((err) => {
-          console.log(err);
-          return null;
-        });
+        return await signInWithEmailAndPassword(auth, credentials.email, credentials.password)
+          .then(user => user.user)
+          .catch(err => {
+            console.log(err);
+            return null;
+          });
       },
     }),
   ],
