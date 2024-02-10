3afea4c9656a8a87d31f51b9e038eb9a9b4a584e
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-02-10 08:48:48 +0300

    feat: integrate search bar

--- src/app/(have-header)/projects/page.tsx
+++ src/app/(have-header)/projects/page.tsx
@@ -1,28 +1,43 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 'use client';
-import { ProjectCard } from '@/common/components';
+import { ProjectCard, SquareProgress } from '@/common/components';
 import { useFetch } from '@/common/hooks';
 import { TGetAllProjects, projectProvider } from '@/provider';
 import { Project } from '@/provider/client';
-import { useEffect } from 'react';
+import { FormEvent, useEffect, useRef } from 'react';
 import { GoSearch } from 'react-icons/go';
 
 const Page = () => {
-  const { fetch: fetchProjects, data: projects } = useFetch<Project[], TGetAllProjects>(projectProvider.getAll);
+  const { fetch: fetchProjects, data: projects, isLoading } = useFetch<Project[], TGetAllProjects>(projectProvider.getAll);
+  const input = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     fetchProjects();
   }, []);
 
+  const handleSearch = () => {
+    if (input.current) {
+      fetchProjects(undefined, input.current.value);
+    }
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    handleSearch();
+  };
+
   return (
     <>
       <div className='flex justify-center items-center  mb-4'>
-        <div className='w-1/3 relative'>
-          <input type='text' placeholder='Search projects' className='input input-bordered bg-white w-full rounded-full' />
-          <span className='absolute active:shadow-sx active:scale-[0.95] cursor-pointer top-1/2 -translate-y-1/2 right-1 text-white bg-secondary p-2 rounded-full shadow-lg'>
-            <GoSearch size={24} />
+        <form className='w-1/3 relative' onSubmit={handleSubmit}>
+          <input ref={input} type='text' placeholder='Search projects' className='input input-bordered bg-white w-full rounded-full' />
+          <span
+            onClick={handleSearch}
+            className='absolute active:shadow-sx active:scale-[0.95] cursor-pointer top-1/2 -translate-y-1/2 right-1 text-white bg-secondary p-2 rounded-full shadow-lg'
+          >
+            {isLoading ? <SquareProgress size={24} /> : <GoSearch size={24} />}
           </span>
-        </div>
+        </form>
       </div>
       <div className='flex justify-center items-start gap-4 flex-wrap'>
         {projects && projects?.length > 0 && projects.map(project => <ProjectCard key={project.id} project={project} />)}
