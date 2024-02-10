7000957839195c3d349214afc14f1895dc89e422
lianafinaritraratovo <hei.finaritra@gmail.com>
2024-02-10 10:30:50 +0300

    chore: redirect user icon to profile

--- src/app/(have-header)/layout.tsx
+++ src/app/(have-header)/layout.tsx
@@ -1,10 +1,14 @@
 'use client';
 import LOGO from '@/assets/kotikota-logo-without-circle.png';
 import { IChildren } from '@/common/components';
+import { PROFILE_PATH } from '@/common/constants/variables';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import { FC } from 'react';
 
 const Layout: FC<IChildren> = ({ children }) => {
+  const { push } = useRouter();
+
   return (
     <div className='w-screen h-screen flex justify-start items-start'>
       <div className='fixed bg-white w-screen h-16 shadow-sm top-0 left-0 flex justify-start items-center'>
@@ -12,7 +16,7 @@ const Layout: FC<IChildren> = ({ children }) => {
           <Image src={LOGO} alt='logo' className='object-contain h-16' />
         </div>
         <div className='grow flex justify-center items-center'></div>
-        <div className='basis-16 flex justify-start items-start'>
+        <div style={{cursor: 'pointer'}} onClick={() => push(PROFILE_PATH)} className='basis-16 flex justify-start items-start'>
           <div className='avatar online placeholder'>
             <div className='bg-neutral text-neutral-content rounded-full w-14'>
               <span className='text-xl text-white'>AI</span>
--- src/common/constants/variables.ts
+++ src/common/constants/variables.ts
@@ -2,3 +2,4 @@ export const HOME_PATH = '/';
 export const PROJECT_PATH = '/projects';
 export const SIGN_IN_PATH = '/signin';
 export const SIGN_UP_PATH = '/signup';
+export const PROFILE_PATH = '/profile'
