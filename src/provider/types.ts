62e7d58e534b2ac78a98d01f61b5d156228e194e
lianafinaritraratovo <hei.finaritra@gmail.com>
2024-02-10 07:07:47 +0300

    feat: integrate user profile updation

--- src/provider/types.ts
+++ src/provider/types.ts
@@ -1,5 +1,5 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { authProvider, projectProvider } from '.';
+import { authProvider, projectProvider, userProvider } from '.';
 
 export type TSignIn = typeof authProvider.signIn;
 export type TSignUp = typeof authProvider.signUp;
@@ -7,3 +7,6 @@ export type TSignUp = typeof authProvider.signUp;
 export type TGetAllProjects = typeof projectProvider.getAll;
 export type TGetOneProject = typeof projectProvider.getOne;
 export type TCreateOrUpdateProject = typeof projectProvider.createOrUpdate;
+
+
+export type TUpdateProfile = typeof userProvider.updateProfile;
